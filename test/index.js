2a98ca899be8bc585e5351d716f1379ac54480d2
tiago-marques <tiago@letsfair.org>
2018-03-09 15:42:59 -0300

    Add test for library

---
+++ test/index.js
@@ -0,0 +1,57 @@
+/***
+ * MIT License
+ *
+ * Copyright (c) 2018 Tiago D'Oliveira Marques dos Santos (tiago-marques)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+const mask = require('../lib/index')
+const assert = require('assert');
+
+describe('one-separator-decimal-mask', function () {
+
+    describe('NaN', function () {
+        it('should return NaN when the value is `test`', function () {
+            assert.deepStrictEqual(mask("test"), NaN);
+        });
+    });
+
+    describe('Last index is not a number', function () {
+        it('should return "10" when the value is `10,0`', function () {
+            assert.equal(mask("10,0"), "10");
+        });
+        it('should return "10" when the value is `10,,`', function () {
+            assert.equal(mask("10,,"), "10");
+        });
+        it('should return "10" when the value is `10..`', function () {
+            assert.equal(mask("10.."), "10");
+        });
+    });
+
+    describe('Must return the input value', function () {
+        it('should return "10," when the value is `10,`', function () {
+            assert.equal(mask("10,"), "10,");
+        });
+        it('should return "10." when the value is `10.`', function () {
+            assert.equal(mask("10."), "10.");
+        });
+    });
+
+});
